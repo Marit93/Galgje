b5f82eedcf7ab88cd3dba80f5a688de1f52d787e
Marit G <maritgerritsen9@gmail.com>
2019-10-04 17:55:34 +0200

    final

--- main.js
+++ main.js
@@ -1,8 +1,34 @@
 let words =["giraffe", "elefant", "cigarette", "lifespan", "javascript", "winc academy", "awkward", "oxygen", "zombie", "jukebox" ];
-let guess;
+
 let guesses = [];
 let lives = [10];
 
 // picks random words from the array "words"
 let randomword = words[Math.floor(Math.random() * words.length)];
 
+for (let i = 0; i <words.length; i++){
+    answer[i] = "_";}
+
+
+while (lives > words.length){
+    let guess = prompt("Guess a letter");
+
+}
+
+comments = function () {
+    showLives.innerHTML = "You have " + lives + " lives";
+    if (lives < 1) {
+      showLives.innerHTML = "Game Over";
+    }
+    for (let i = 0; i < guesses.length; i++) {
+      if (counter + space === guesses.length) {
+        showLives.innerHTML = "You Win!";
+      }
+    }
+  }
+
+
+  let $guessedLetters = document.getElementById("guessed-letters");
+  let $leftGuessesCounter = document.getElementById("guesses");
+  let $currentWord = document.getElementById("current-word");
+  let $putLetter = document.getElementById("putLetter");
