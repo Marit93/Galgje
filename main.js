d6f6b704e9ac6c1609ca245c1c9b565048cbbb5f
Marit G <maritgerritsen9@gmail.com>
2019-10-04 15:56:26 +0200

    changes made to js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 let words =["giraffe", "elefant", "cigarette", "lifespan", "javascript", "winc academy", "awkward", "oxygen", "zombie", "jukebox" ];
 let guess;
 let guesses = [];
-let lives;
+let lives = [10];
 
 // picks random words from the array "words"
 let randomword = words[Math.floor(Math.random() * words.length)];
+
